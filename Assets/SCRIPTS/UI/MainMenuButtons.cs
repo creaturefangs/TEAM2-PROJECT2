8905f266236c2d77c02fb5335ccccdd94e72b898
creaturefangs <126419720+creaturefangs@users.noreply.github.com>
2023-09-26 20:37:48 -0400

    ui push

---
+++ Assets/SCRIPTS/UI/MainMenuButtons.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void OnQuitButtonClick()
+    {
+        Application.Quit();
+    }
+
+    public void OnHelpButtonClick()
+    {
+        SceneManager.LoadScene("HelpScene");
+    }
+
+    public void OnCreditButtonClick()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void OnMainMenuButtonClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OnClickPlaytest()
+    {
+        SceneManager.LoadScene("PLAYTEST");
+    }
+
+    public void OnClickCutsceneOne()
+    {
+        SceneManager.LoadScene("INTROCUTSCENE");
+    }
+
+    public void OnClickLoadScreen()
+    {
+        SceneManager.LoadScene("LoadingScreen");
+    }
+}
