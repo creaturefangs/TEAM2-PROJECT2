363af173bd2f4fd90414338cd1229bb66822963f
creaturefangs <126419720+creaturefangs@users.noreply.github.com>
2023-10-06 10:09:57 -0400

    level one and help menu push

---
+++ Assets/SCRIPTS/ENVIRONMENT/AudioManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+
+{
+    public AudioSource audioManager;
+    public AudioClip[] Music;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayMusic()
+    {
+
+         // checks if player is colliding with a music box cllider, and if so, play a certain abient soundtrack. 
+         // if the player is not colliding with a certain box collider, the base music plays
+    }
+}
