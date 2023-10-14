3588d2c9f607b012d033f957a6e5c193087e5576
aldenchappellWT <118118263+aldenchappell@users.noreply.github.com>
2023-10-14 11:34:11 -0400

    Collisions and Pushable objects (lvl1)

--- Assets/SCRIPTS/ENVIRONMENT/ItemPickupComponent.cs
+++ Assets/SCRIPTS/ENVIRONMENT/ItemPickupComponent.cs
@@ -8,9 +8,10 @@ public class ItemPickupComponent : MonoBehaviour
     public ParticleSystem particles; //particles when item is picked up
     public GameObject itemUIImage; //UIImage when item is picked up
     public bool enableDestructionOnPickup = true; //destroy item when its picked up?
+    
     public void OnItemPickup()
     {
-        ParticleSystem particleObj = Instantiate(particles, transform.position, Quaternion.identity);
-        Destroy(particleObj, .25f);
+        //ParticleSystem particleObj = Instantiate(particles, transform.position, Quaternion.identity);
+        //Destroy(particleObj, .25f);
     }
 }
