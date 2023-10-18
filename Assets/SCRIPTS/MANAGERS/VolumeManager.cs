c94517d88c97704dbeae067992ecbd70845e0215
aldenchappellWT <118118263+aldenchappell@users.noreply.github.com>
2023-10-18 01:10:24 -0400

    Audio settings and fixed player attacking

--- Assets/SCRIPTS/MANAGERS/VolumeManager.cs
+++ Assets/SCRIPTS/MANAGERS/VolumeManager.cs
@@ -1,47 +1,45 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class VolumeManager : MonoBehaviour
 {
-    [SerializeField] private TMP_Text volumeText;
-    [SerializeField] private Slider slider;
+    [SerializeField]
+    private TMP_Text volumeText;
+    [SerializeField]
+    private Slider slider;
+    
+    [SerializeField]
+    private string volumeParameter = "volume";
+    [SerializeField]
+    private AudioMixer mixer;
+    [SerializeField]
+    private float multiplier = 30.0f;
 
-    [SerializeField] private string volumeParameter;
-    [SerializeField] private AudioMixer mixer;
-    [SerializeField] private float multiplier = 30.0f;
+    private float currentVolume;
 
     private void Awake()
     {
+        // Retrieve volume from PlayerPrefs or use a default value of 0.75f.
+        currentVolume = PlayerPrefs.GetFloat(volumeParameter, 0.75f);
+        slider.value = currentVolume;
+
+        // Set volume in AudioMixer to match the slider value.
+        mixer.SetFloat(volumeParameter, Mathf.Log10(currentVolume) * multiplier);
+
+        // Set text to match the slider value.
+        volumeText.text = slider.value.ToString("f1");
+
         slider.onValueChanged.AddListener(HandleSliderValueChanged);
     }
-    
-    private void OnEnable()
-    {
-        SetVolume();
-    }
 
-    private void OnDisable()
-    {
-        SetVolume();
-    }
-    
-    void Start()
-    {
-        slider.value = PlayerPrefs.GetFloat(volumeParameter, slider.value);
-    }
-    
     private void HandleSliderValueChanged(float value)
     {
-        mixer.SetFloat(volumeParameter, Mathf.Log10((value)) * multiplier);
+        // Every time the slider value changes, update the volume in PlayerPrefs and AudioMixer.
+        mixer.SetFloat(volumeParameter, Mathf.Log10(value) * multiplier);
         volumeText.text = slider.value.ToString("f1");
-    }
-
-    private void SetVolume()
-    {
-        PlayerPrefs.SetFloat(volumeParameter, slider.value);
-        slider.value = PlayerPrefs.GetFloat(volumeParameter);
+        PlayerPrefs.SetFloat(volumeParameter, value);
+        PlayerPrefs.Save();  // Immediately save PlayerPrefs after updating.
     }
 }
